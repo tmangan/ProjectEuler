9b71718f92a29f0925e942d8e319b12b7ee0c52a
Thomas <64053582+tmangan@users.noreply.github.com>
2022-06-08 01:21:41 +0000

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+def solve():
+    """
+        Adds the numbers divisible by those in div_set
+        up to (not including) high_val
+    """
+
+    divisible_numbers = [3, 5]
+    highest_value = 1000
+    running_total = 0
+
+    for i in range(highest_value):
+
+        truth_table = []
+        for j in divisible_numbers:
+            if i % j == 0:
+                truth_table.append(True)
+            else:
+                truth_table.append(False)
+        if any(truth_table):
+            running_total += i
+
+    return running_total
+
+
+if __name__ == "__main__":
+    print(solve())
