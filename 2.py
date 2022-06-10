5238331fc4f87e12ce27f7589eabd8b0bc560b3d
Thomas <64053582+tmangan@users.noreply.github.com>
2022-06-10 21:50:31 +0000

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,13 +5,17 @@ def solve(largest_n):
     """
         returns the sum of even valued fibonacci numbers up to largest_n
     """
-    running_total = 0
+    
+    running_total = 0  # Initialization of variables
     nth_fib = 1
     n_plus_one_fib = 2
 
     while nth_fib <= largest_n:
+        
         if nth_fib % 2 == 0:
             running_total += nth_fib
+        
+        # Cycle the Fibonacci Sequence one term
         nth_fib, n_plus_one_fib = n_plus_one_fib, n_plus_one_fib + nth_fib
 
     return running_total
